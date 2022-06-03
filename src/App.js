96447512d1d816ddfa5ec6c30df8db53be428469
Fall-Snowy <87006716+Fall-Snowy@users.noreply.github.com>
2022-06-03 13:05:20 +0800

    commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import TodoList from './TodoList';
 import './App.css';
+import { useState, useRef } from 'react';
+import { v4 as uuidv4} from 'uuid'; //自動gen 不同的ID
 
 function App() {
+  const [todos, setTodos] = useState([
+    {id:1,name:"Todo1",completed:false}
+  ]);
+
+const todoNameRef = useRef();
+
+const handleAddTodo = ()=>{
+  const name = todoNameRef.current.value;
+  if (name === "") return;
+  setTodos((prevTodos)=>{
+    return [...prevTodos,{id:uuidv4(),name:name,completed:false}];
+  });
+  todoNameRef.current.value = null;
+};
+
+const toggleTodo = (id) =>{
+  const newTodos = [...todos];
+  const todo = newTodos.find((todo) => todo.id === id);
+  todo.completed = !todo.completed;
+  setTodos(newTodos);
+}
+
+const handleClear = () => {
+  const newTodos = todos.filter((todo)=> !todo.completed);
+  setTodos(newTodos);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      <input type="text" ref={todoNameRef} />
+      <button onClick={handleAddTodo}>Task insert</button>
+      <button onClick={handleClear}>Delete Task</button>
+      <div>Have Task: {todos.filter((todo)=> !todo.completed).length}</div>
     </div>
-  );
+    
+  )
 }
 
 export default App;
